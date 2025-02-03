49b95395bc8bce2d2c385221b687fb24e46d4e58
CrisAti <daniati2004@hotmail.com>
2025-02-03 09:24:15 -0500

    Update conexion.js

--- conexion/conexion.js
+++ conexion/conexion.js
@@ -1,7 +1,7 @@
 import mongoose  from "mongoose";
 export const conexion=async () =>{
    try{
-     await mongoose.connect("mongodb://localhost:27017");
+     await mongoose.connect("mongodb+srv://daniati:1234@clustertest.o2jt8.mongodb.net/");
      console.log ("conectado correctamente al mongo");
 
    }
